5d36666525269df66be1afeab9963426c5a88722
piotrmikula <p.mikula14@wp.pl>
2021-01-02 15:52:25 +0000

    Zadanie3

--- lab7.cpp
+++ lab7.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <variant>
 #include <vector>
 
 int main()
 {
-    std::cout << "Podaj liczbe calkowita: ";
-    int n;
-    std::cin >> n;
+    std::cout << "Podaj liczbe calkowita a: ";
+    int a;
+    std::cin >> a;
+
+    std::cout << "Podaj liczbe calkowita b: ";
+    int b;
+    std::cin >> b;
+
     try {
-        std::vector< int > vec(n, 0);
+        std::vector< int >                      vec(a, 0);
+        std::variant< int, float, std::string > var;
+        if (b % 2 == 0)
+            var = 42;
+        else
+            var = "nieparzyste";
+        std::cout << std::get< int >(var) << std::endl;
     }
     catch (const std::bad_alloc& ba) {
         std::cout << "bad_alloc caught: " << ba.what() << '\n';
+    }
+    catch (const std::bad_variant_access& bva) {
+        std::cout << "bad_variant_access: " << bva.what() << '\n';
     }
 
     puts("\nOstatnia linijka kodu!");
