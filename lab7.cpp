4c6f9912bfc2ebaac8106c80862ab907d28ec01b
piotrmikula <p.mikula14@wp.pl>
2021-01-02 15:39:26 +0000

    Zadanie1

--- lab7.cpp
+++ lab7.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    puts("Hello, World!");
+    std::cout << "Podaj liczbe calkowita: ";
+    int n;
+    std::cin >> n;
+    try {
+        std::vector< int > vec(n, 0);
+    }
+    catch (...) {
+        std::cout << "Wyjatek!\n";
+    }
+
+    puts("\nOstatnia linijka kodu!");
 }
